e295b64e47e34b739bdb697f2584187facedfda8
Carlos Jaramillo <cejaramillof@gmail.com>
2017-09-07 12:43:52 -0500

    Form builders 3

--- app/form_builders/bootstrap_form_builder.rb
+++ app/form_builders/bootstrap_form_builder.rb
@@ -4,14 +4,31 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
   [:text_field,:text_area,:url_field,:email_field,:number_field].each do |metodo|
     define_method metodo do |name,*args|
       options = args.extract_options!
+      aditional_classes = "form-control floating-label"
       if options.has_key?(:class)
-        options[:class] += "form-control"
+        options[:class] += aditional_classes
       else
-        options[:class] = "form-control"
+        options[:class] = aditional_classes
       end
+      options[:placeholder] = name.capitalize
       content_tag :div, class:"form-group" do
-        content_tag(:label,name, name: name) + super(name,options)
+        super(name,options)
       end 
+    end
+  end
+  
+  def errors
+    if object.errors.any?
+      content_tag :div, class:"red large-padding white-text" do
+        content_tag(:h2, "Errores") +
+        content_tag(:ul) do
+          lis = ""
+          object.errors.full_messages.each do |err|
+            lis += content_tag :li, err
+          end
+          lis.html_safe
+        end
+      end
     end
   end
   
