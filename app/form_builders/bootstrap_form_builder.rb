68d73e5f60519d1e73c2c0d044a3d1e08902ac52
Carlos Jaramillo <cejaramillof@gmail.com>
2017-09-07 12:06:54 -0500

    Form builders 1

---
+++ app/form_builders/bootstrap_form_builder.rb
@@ -0,0 +1,33 @@
+class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
+  delegate :content_tag, to: :@template
+  
+  def text_field name, *args
+    options = args.extract_options!
+    if options.has_key?(:class)
+      options[:class] += "form-control"
+    else
+      options[:class] = "form-control"
+    end
+    content_tag :div, class:"form-group" do
+      content_tag(:label,name, name: name) + super(name,options)
+    end
+  end
+  
+  def text_area name, *args
+    options = args.extract_options!
+    if options.has_key?(:class)
+      options[:class] += "form-control"
+    else
+      options[:class] = "form-control"
+    end
+    content_tag :div, class:"form-group" do
+      content_tag(:label,name, name: name) + super(name,options)
+    end
+  end
+  
+  def submit(*args)
+    content_tag :div do
+      super(*args,class:"btn btn-success")
+    end
+  end
+end
