410cffb09690404b35e2d8518483155f65de2a1d
Carlos Jaramillo <cejaramillof@gmail.com>
2017-09-08 10:51:07 -0500

    Cobrar con paypal generando cobro

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  
+
   resources :attachments, only: [:create,:destroy,:new,:show]
   resources :products
   resources :in_shopping_carts, only: [:create,:destroy]
   devise_for :users
   
   post "/emails/create", as: :create_email
+  post "/pagar", to: "payments#create"
   
   get "/carrito", to: "shopping_carts#show"
   get "/add/:product_id", as: :add_to_cart,to: "in_shopping_carts#create"
