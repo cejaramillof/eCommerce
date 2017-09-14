11276f57ec496470e4e4c4eb772557ee4387bbae
Carlos Jaramillo <cejaramillof@gmail.com>
2017-09-14 06:39:46 -0500

    Ventas por producto

--- app/models/product.rb
+++ app/models/product.rb
@@ -18,6 +18,9 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :attachments
+  has_many :in_shopping_carts
+  has_one :shopping_cart, through: :in_shopping_carts
+  has_many :my_payments, through: :shopping_cart
   validates_presence_of :name, :user, :pricing
   validates :pricing, numericality: { greater_than: 0}
   has_attached_file :avatar, styles: { medium: "300x300", thumb:"100x100" }, default_url: "missing.png"
@@ -25,5 +28,13 @@ class Product < ActiveRecord::Base
   
   def paypal_form
     {name: name,sku: :item, price: (pricing/100),currency:"USD",quantity:1}
+  end
+  
+  def sales
+    my_payments.payed
+  end
+  
+  def sales_total
+    my_payments.payed.count * self.pricing
   end
 end
