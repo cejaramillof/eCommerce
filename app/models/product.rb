0a981341c1350a4b96186190bfccec5e8e9a3e73
Carlos Jaramillo <cejaramillof@gmail.com>
2017-09-17 11:41:51 -0500

    add estadisticas

--- app/models/product.rb
+++ app/models/product.rb
@@ -27,6 +27,10 @@ class Product < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300", thumb:"100x100" }, default_url: "missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
+  def self.search(search)
+    where("name ILIKE ? OR description ILIKE ?", "%#{search}%", "%#{search}%") 
+  end
+  
   def paypal_form
     {name: name,sku: :item, price: (pricing),currency:"USD",quantity:1}
   end
